fc44dfc5f53addf8fe9ba9694691fd00e960c5cc
Jhoanna Lagumbay <jhoanna@mytdevcorp.com>
2022-06-10 00:39:07 +0800

    diagnosiis controller

--- backend/controller/diagnosisController.js
+++ backend/controller/diagnosisController.js
@@ -36,27 +36,54 @@ const createDiagnosis = asyncHandler(async (req, res) => {
   res.status(200).json(newDiagnosis)
 })
 
+// @desc  get all diagnosis
+// @route POST emr/api/diagnosis/getAll
+// access Private
+const getAllDiagnosis = asyncHandler(async (req, res) => {
+  const diagnosis = await Diagnosis.find()
+  
+  if(!diagnosis){
+    res.status(400)
+    throw new Error("Diagnosis not found")
+  }
+
+
+  res.status(200).json(diagnosis)
+})
 // @desc  get diagnosis
 // @route POST emr/api/diagnosis/get/:id
 // access Private
 const getDiagnosis = asyncHandler(async (req, res) => {
-  const diagnosis = await Patient.findById(req.params.id)
+  const diagnosis = await Diagnosis.findById(req.params.id)
   
-  if(!patient){
+  if(!diagnosis){
     res.status(400)
-    throw new Error("patient not found")
+    throw new Error("Diagnosis not found")
   }
 
 
   res.status(200).json({ 
-    _id: patient._id,
-    name: patient.name,
-    b_day: patient.b_day,
-    sex: patient.sex,
-    emergency_name: patient.emergency_name,
-    emergency_no: patient.emergency_no
+    _id: diagnosis._id,
+    patient_id: diagnosis.patient_id,
+    doctor_id:diagnosis.doctor_id,
+    diagnosis:diagnosis.diagnosis,
+    prescription:diagnosis.prescription,
   })
+})
+// @desc  get diagnosis
+// @route POST emr/api/diagnosis/get/:id
+// access Private
+const getDiagnosisPerPatient = asyncHandler(async (req, res) => {
+  const diagnosis = await Diagnosis.find({patient_id:req.params.id})
+  
+  if(!diagnosis){
+    res.status(400)
+    throw new Error("diagnosis not found")
+  }
+
+
+  res.status(200).json(diagnosis)
 })
 
 
-module.exports = {createDiagnosis}
+module.exports = {createDiagnosis, getDiagnosis, getDiagnosisPerPatient, getAllDiagnosis}
--- backend/routes/diagnosisRoutes.js
+++ backend/routes/diagnosisRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const {createDiagnosis} = require('../controller/diagnosisController.js')
+const {createDiagnosis, getDiagnosis, getDiagnosisPerPatient, getAllDiagnosis} = require('../controller/diagnosisController.js')
 
 router.post('/create', createDiagnosis)
+router.post('/getAll', getAllDiagnosis)
+router.post('/get/:id', getDiagnosis)
+router.post('/getPerPatient/:id', getDiagnosisPerPatient)
 
 
 module.exports  = router
