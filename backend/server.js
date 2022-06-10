9bd43c620b53d60d820569bc69beb39ddb65bbda
Jhoanna Lagumbay <jhoanna@mytdevcorp.com>
2022-06-10 18:34:46 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,12 @@ connectDB()
 
 const app = express()
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
