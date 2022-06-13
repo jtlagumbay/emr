4347d2fbce4e2d4b07a779610677bedd960fb3dd
Jhoanna Lagumbay <jhoanna@mytdevcorp.com>
2022-06-13 23:13:51 +0800

    patient details layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DoctorLogin from "./pages/doctorlogin/DoctorLogin.js";
 import DoctorMenu from "./pages/doctormenu/DoctorMenu.js";
 import Home from "./pages/home/Home.js";
+import PatientDetails from "./pages/patient/PatientDetails.js";
 import PatientLogin from "./pages/patient/PatientLogin.js";
 import PatientSignup from "./pages/patient/PatientSignup.js";
 
@@ -19,6 +20,7 @@ export default function App() {
           <Route path="/doctor/menu" element={<DoctorMenu />} />
           <Route path="/patient/login" element={<PatientLogin />} />
           <Route path="/patient/signup" element={<PatientSignup />} />
+          <Route path="/patient/details" element={<PatientDetails />} />
         </Routes>
       </Router>
     </div>
---
+++ frontend/src/components/History.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function History({data, headers}) {
+
+  const rows = data.map((row, index)=>{
+    let rowData = [];
+    let i = 0;
+    
+    for(const key in row) {
+        rowData.push({
+            key: headers[i],
+            val: row[key]
+        });
+        i++;
+    }
+
+    return <tr key={row.id}>
+      {rowData.map((data, index) => 
+      <td key={index} data-heading={data.key}>{data.val}</td>)}
+      <td key={index+"button"} ><button className="his-button">View Details</button></td>
+    </tr>
+
+  })
+
+  return (
+    <div className="section">
+      <h3 className='section-title m-0'>Medical History</h3>
+      <br/>
+      <table className="table table-lg table-responsive">
+        <thead>
+          <tr>
+              {headers.map((col,index) => (
+                  <th key={index} className="his-header">{col}</th>
+              ))}
+          </tr>
+        </thead>
+        <tbody>
+            {rows}
+        </tbody>
+      </table>
+    </div>
+  )
+}
