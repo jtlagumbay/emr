53c5e6df8455dc70f1bcd07b60cd48f9dd22d30f
pmxfa <pamarfa@up.edu.ph>
2022-06-15 21:30:13 +0800

    Create PatientDiagnosis.js

---
+++ frontend/src/pages/patient/PatientDiagnosis.js
@@ -0,0 +1,59 @@
+export default function PatientDetails() {
+    document.body.style = 'background: white;';
+    const [patientInfo, setPatientInfo] = useState({
+      name: "Juana Dela Cruz",
+      bday: "Jun 1, 2022",
+      age:  "20",
+      sex: "Female",
+      contact_no: "09123445",
+      emergency_name: "Teodora",
+      emergency_contact: "091234"
+    })
+  
+    const [history, setHistory] = useState([
+      {
+        date:"May 1, 2022",
+        doc: "Dr. Steven Strange",
+        diagnosis: "Csadfakhfadlfh" 
+      },    
+      {
+        date:"May 1, 2022",
+        doc: "Dr. Steven Strange",
+        diagnosis: "Csadfakhfadlfh" 
+      },    
+      {
+        date:"May 1, 2022",
+        doc: "Dr. Steven Strange",
+        diagnosis: "Csadfakhfadlfh" 
+      },    
+      {
+        date:"May 1, 2022",
+        doc: "Dr. Steven Strange",
+        diagnosis: "Csadfakhfadlfh" 
+      },
+    ])
+  
+  
+  
+  
+    return (
+      <div className='container'>
+        <br/>
+        <PatientInfo 
+          data = {patientInfo}
+          button={"Edit Profile"}
+        />
+        <br/>
+        <br/>
+        
+        <History 
+          data = {history}
+          headers = {["DATE", "DOCTOR", "DIAGNOSIS", ""]}
+        />
+          {/* Medical Records */}
+        <br/>
+        <br/>
+      </div>
+    )
+  }
+  
