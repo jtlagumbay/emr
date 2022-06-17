875b54de0e92478c1e4d2fd92c5a2a9cea71b4a4
Jhoanna Lagumbay <jhoanna@mytdevcorp.com>
2022-06-17 10:43:01 +0800

    deploy

--- backend/server.js
+++ backend/server.js
@@ -39,11 +39,11 @@ if(process.env.NODE_ENV=== 'production'){
 app.use(errorHandler)
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static("public"))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
 app.listen(port, ()=>{console.log(`listening to port ${port}`)})
